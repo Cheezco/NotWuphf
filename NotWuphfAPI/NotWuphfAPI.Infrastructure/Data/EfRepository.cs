4b62eb68e9697ebb504750fbff1bba4cc0c75518
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-10-07 10:35:30 +0300

    Add infrastructure

---
+++ NotWuphfAPI/NotWuphfAPI.Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Specification;
+using Ardalis.Specification.EntityFrameworkCore;
+using NotWuphfAPI.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWuphfAPI.Infrastructure.Data
+{
+    public class EfRepository<T> : RepositoryBase<T>, IRepository<T> where T : class
+    {
+        public EfRepository(MainContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
---
+++ NotWuphfAPI/NotWuphfAPI.Infrastructure/Dependencies.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NotWuphfAPI.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWuphfAPI.Infrastructure
+{
+    public static class Dependencies
+    {
+        public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            var useOnlyInMemoryDatabase = false;
+            if (configuration["useOnlyInMemoryDatabase"] is not null)
+            {
+                useOnlyInMemoryDatabase = bool.Parse(configuration["useOnlyInMemoryDatabase"]);
+            }
+
+            if (useOnlyInMemoryDatabase)
+            {
+                services.AddDbContext<MainContext>(x =>
+                    x.UseInMemoryDatabase("Main"));
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
