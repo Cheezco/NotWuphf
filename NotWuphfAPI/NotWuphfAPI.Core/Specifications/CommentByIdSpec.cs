cd7fc8ebef98fdb5e70a59fbc9a07ac4ef32c92d
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-10-07 11:01:43 +0300

    Add specifications

---
+++ NotWuphfAPI/NotWuphfAPI.Core/Specifications/CommentByIdSpec.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Specification;
+using NotWuphfAPI.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWuphfAPI.Core.Specifications
+{
+    public class CommentByIdSpec : Specification<Comment>, ISingleResultSpecification<Comment>
+    {
+        public CommentByIdSpec(int id)
+        {
+            Query
+                .Where(x => x.Id == id);
+        }
+    }
+}
---
+++ NotWuphfAPI/NotWuphfAPI.Core/Specifications/PostByIdSpec.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Specification;
+using NotWuphfAPI.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWuphfAPI.Core.Specifications
+{
+    public class PostByIdSpec : Specification<Post>, ISingleResultSpecification<Post>
+    {
+        public PostByIdSpec(int id)
+        {
+            Query
+                .Where(x => x.Id == id);
+        }
+    }
+}
