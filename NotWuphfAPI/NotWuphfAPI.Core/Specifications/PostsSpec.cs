bd7f1e23904a742cd8892d115ed16f924c3d58e2
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-10-07 10:57:39 +0300

    Add specifications

---
+++ NotWuphfAPI/NotWuphfAPI.Core/Specifications/PostsSpec.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.Specification;
+using NotWuphfAPI.Core.Entities;
+using NotWuphfAPI.Core.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWuphfAPI.Core.Specifications
+{
+    public class PostsSpec : Specification<Post>
+    {
+        public PostsSpec(int page = PaginationHelper.DefaultPage, int pageSize = PaginationHelper.DefaultPageSize)
+        {
+            Query
+                .Skip(PaginationHelper.CalculateSkip(pageSize, page))
+                .Take(PaginationHelper.CalculateTake(pageSize));
+        }
+    }
+}
