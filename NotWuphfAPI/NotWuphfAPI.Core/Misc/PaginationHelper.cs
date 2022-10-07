c061f3264e09cbb47d32e663e7a6e2f59d4ba2ec
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-10-07 10:49:57 +0300

    Add pagination helper

---
+++ NotWuphfAPI/NotWuphfAPI.Core/Misc/PaginationHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWuphfAPI.Core.Misc
+{
+    public class PaginationHelper
+    {
+        public const int DefaultPage = 1;
+        public const int DefaultPageSize = 10;
+
+        public static int CalculateTake(int pageSize)
+        {
+            return pageSize <= 0 ? DefaultPageSize : pageSize;
+        }
+
+        public static int CalculateSkip(int pageSize, int page)
+        {
+            page = page <= 0 ? DefaultPage : page;
+
+            return CalculateTake(pageSize) * (page - 1);
+        }
+    }
+}
