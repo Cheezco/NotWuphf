40d5dcf2e47355bea620a22e5809bcf7c35a5d8a
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-12-15 21:09:53 +0200

    Add migration

---
+++ NotWuphfAPI/NotWuphfAPI.Infrastructure/Migrations/20221215184855_Group membership removal.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotWuphfAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Groupmembershipremoval : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GroupWuphfUser");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GroupWuphfUser",
+                columns: table => new
+                {
+                    GroupsId = table.Column<int>(type: "integer", nullable: false),
+                    MembersId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupWuphfUser", x => new { x.GroupsId, x.MembersId });
+                    table.ForeignKey(
+                        name: "FK_GroupWuphfUser_AspNetUsers_MembersId",
+                        column: x => x.MembersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GroupWuphfUser_Groups_GroupsId",
+                        column: x => x.GroupsId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupWuphfUser_MembersId",
+                table: "GroupWuphfUser",
+                column: "MembersId");
+        }
+    }
+}
