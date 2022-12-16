579662aca4936e977e41fe439ad9b0565f3fdf68
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-12-16 10:08:55 +0200

    Display username

--- NotWuphfFront/notwuphf/components/layouts/mainLayout/navUser.tsx
+++ NotWuphfFront/notwuphf/components/layouts/mainLayout/navUser.tsx
@@ -1,13 +1,17 @@
 import styles from "styles/shared/components/mainLayout/nav/navUser.module.css";
 import { Icon } from "@chakra-ui/react";
 import { FiLogOut } from "react-icons/fi";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import WuphfUser from "../../../types/WuphfUser";
 
 export default function NavUser() {
+  const { data: session } = useSession();
+  const user = session?.user as WuphfUser;
+
   return (
     <div className={styles.navUserOuter}>
       <div className={styles.navUser}>
-        <div>Username</div>
+        <div>{user.username}</div>
       </div>
       <button className={styles.navLogout} onClick={() => signOut()}>
         <Icon fontSize="22px" as={FiLogOut} />
