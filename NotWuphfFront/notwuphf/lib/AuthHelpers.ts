fa6a601e5d5fb38087431c16f0cc0ec25481bb26
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-12-16 10:16:23 +0200

    Add getRole function

--- NotWuphfFront/notwuphf/lib/AuthHelpers.ts
+++ NotWuphfFront/notwuphf/lib/AuthHelpers.ts
@@ -9,4 +9,15 @@ function isAdmin(user: WuphfUser) {
   return user.role.filter((x) => x === "Admin").length !== 0;
 }
 
+function getRoles(user: WuphfUser): string[] {
+  if (typeof user === "undefined" || typeof user.role === "undefined")
+    return [];
+
+  if (typeof user.role === "string") {
+    return [user.role];
+  }
+
+  return user.role;
+}
+
 export { isAdmin };
